e7ceb290870302670360840aa5e4c7d6ef7ebd28
Sai1Ram <sairamsenapati0022@gmail.com>
2026-06-02 07:56:21 +0530

    feat: integrate Meta Pixel tracking code into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-18052182389"
         ></Script>
-        <Script>
+        <Script id="google-analytics">
           {`
     window.dataLayer = window.dataLayer || [];
     function gtag(){window.dataLayer.push(arguments);}
@@ -41,6 +41,32 @@ export default function RootLayout({
     gtag('config', 'AW-18052182389');
   `}
         </Script>
+        {/* <!-- Meta Pixel Code --> */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1514114043481607');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1514114043481607&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* <!-- End Meta Pixel Code --> */}
       </head>
       <body className={` ${geistMono.className} antialiased bg-[#FAF8FF]`}>
         <Suspense fallback={null}>
